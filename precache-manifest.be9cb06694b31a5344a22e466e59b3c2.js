2805271642b6f12e74c0d8e8d4460d7e5c19a83d
= <oleksandr.kononov@errigal.com>
2019-09-12 16:24:36 +0100

    Updates

---
+++ precache-manifest.be9cb06694b31a5344a22e466e59b3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318d54ce6a4b00b5ececd19d3f5e55d4",
+    "url": "/RandomCats/index.html"
+  },
+  {
+    "revision": "fdfbe629641b8f18ddef",
+    "url": "/RandomCats/static/css/main.2be37466.chunk.css"
+  },
+  {
+    "revision": "01b7390ff6eadc015856",
+    "url": "/RandomCats/static/js/2.01e6883c.chunk.js"
+  },
+  {
+    "revision": "fdfbe629641b8f18ddef",
+    "url": "/RandomCats/static/js/main.afd9da24.chunk.js"
+  },
+  {
+    "revision": "25560249900a7592dee4",
+    "url": "/RandomCats/static/js/runtime~main.57cf0b32.js"
+  },
+  {
+    "revision": "71cf9171f184681b898090ed335acd48",
+    "url": "/RandomCats/static/media/background.71cf9171.jpg"
+  }
+]);
